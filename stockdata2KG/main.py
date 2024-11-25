11d9fe5521e9123de6b3bb9b290d2814d4aebe25
jakobschmidbauer <ge75div@mytum.de>
2024-11-25 14:07:29 +0100

    made main fuction company searchable

--- stockdata2KG/main.py
+++ stockdata2KG/main.py
@@ -2,7 +2,7 @@ from stockdata2KG.crawler import crawl_wikidata
 
 def main():
      # just input a company, e.g. "Apple Inc" or "Allianz SE"
-     crawl_wikidata("Apple Inc")
+     crawl_wikidata("Allianz SE")
 
 
 if __name__ == "__main__":
