039cf04fe6640e1b6c5a8d078cb5fa945234544c
jakobschmidbauer <ge75div@mytum.de>
2024-11-30 18:31:39 +0100

    -

--- stockdata2KG/main.py
+++ stockdata2KG/main.py
@@ -4,15 +4,25 @@ from neo4j import GraphDatabase
 import json
 from stockdata2KG.crawler import crawl_wikidata
 from stockdata2KG.fill_template import fill_template
-from stockdata2KG.graphbuilder import initialize_graph, neo4j_uri
+#from stockdata2KG.llm import process_article_and_update_graph, connect_to_neo4j
 from stockdata2KG.wikidata import wikidata_wbsearchentities, wikidata_wbgetentities
 
 
 def main():
      # just input a company, e.g. "Apple Inc" or "Allianz SE"
-     id_of_company = wikidata_wbsearchentities("Apple Inc", 'id')
+     id_of_company = wikidata_wbsearchentities("Microsoft", 'id')
      wikidata = wikidata_wbgetentities(id_of_company)
      fill_template(id_of_company, wikidata)
+
+    # graph = connect_to_neo4j("bolt://localhost:7687", "neo4j", "neo4jtest")
+
+     # Example article text
+     article_text = """
+         Example News 
+         """
+
+     # Process the article and update the graph
+     #process_article_and_update_graph(graph, article_text)
 
 
 if __name__ == "__main__":
